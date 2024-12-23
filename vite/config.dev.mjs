5e7494f1b6cce0151faa3b74100415b207bd99e4
Zhenya Kolyaga <e.kolyaga@harddays.dev>
2024-12-23 18:50:31 +0300

    add deploy settings

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
+    base: "/arkanoid/",
     plugins: [react()],
     server: {
         port: 8080,
         host: true,
+    },
+    build: {
+        outDir: "dist",
     },
 });
