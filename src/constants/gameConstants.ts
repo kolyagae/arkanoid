9c9b30f2717203d9ba1e79f26392d9a8fd813f47
Zhenya Kolyaga <e.kolyaga@harddays.dev>
2024-12-23 18:59:13 +0300

    Updates

--- src/constants/gameConstants.ts
+++
@@ -1,7 +0,0 @@
-const gameConstants = {
-    basePoints: 10,
-    startingLives: 3,
-    clearMultiplier: 2,
-};
-
-export default gameConstants;
--- src/constants/physicsConstants.ts
+++
@@ -1,6 +0,0 @@
-const physicsConstants = {
-    speedMultiplier: 1.08,
-    baseBallVelocity: 300,
-};
-
-export default physicsConstants;
